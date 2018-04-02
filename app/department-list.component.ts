f36e47c233b290f7bbaf435cb5ed0da6cd4ad3b9
Erald Bulku <erald.b@scopicsoftware.com>
2018-04-02 16:21:59 +0200

    Small fix

--- app/department-list.component.ts
+++ app/department-list.component.ts
@@ -26,7 +26,7 @@ export class DepartmentListComponent implements OnInit {
 
 	onSelect(department){
 		//this.router.navigate(['/departments', department.id]);
-		this.router.navigate([department.id, {relativeTo: this.route}]);
+		this.router.navigate([department.id], {relativeTo: this.route});
 	}
 
 	ngOnInit(){
